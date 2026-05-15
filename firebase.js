edbafbaa17ac26e39608be714d51b0e60cf3b37a
christian2006yan-design <christian2006yan@gmail.com>
2026-05-15 21:57:32 +0800

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBukgYgCfvIi_Z8CJ8PQi2TiHkXwbp2GEg",
+  authDomain: "pcds-booking-6727c.firebaseapp.com",
+  projectId: "pcds-booking-6727c",
+  storageBucket: "pcds-booking-6727c.firebasestorage.app",
+  messagingSenderId: "193663771576",
+  appId: "1:193663771576:web:413ec2d11294412e97a54d"
+};
+
+export const app = initializeApp(firebaseConfig);
